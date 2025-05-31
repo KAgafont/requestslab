6d24c89bfb1214c4cfd98aeb14347b6abc54877d
KAgafont <k.agafontsev@.com>
2025-05-31 08:07:55 +0500

    update

--- T1.py
+++ T1.py
@@ -3,6 +3,12 @@ import re
 from collections import Counter
 import time
 
+ips2 = [
+    "81.30.83.98",
+    "194.84.46.110",
+    "37.192.247.194",
+    "31.246.103.203"
+]
 
 def get_html(url):
     headers = {
@@ -37,14 +43,7 @@ def get_country_by_ip(ip):
         print(f"Ошибка при запросе для IP {ip}: {e}")
         return None
 
-
-def main():
-    url = "https://ru.wikipedia.org/w/index.php?title=JSON&action=history"
-    html = get_html(url)
-
-    ips = extract_ips(html)
-    print(f"Найдено уникальных IP: {len(ips)}")
-
+def show(ips):
     country_counter = Counter()
 
     for ip in ips:
@@ -54,11 +53,23 @@ def main():
             print(f"{ip} -> {country}")
         else:
             print(f"{ip} -> Страна не определена")
-        time.sleep(1)  # чтобы не перегружать API
+        time.sleep(1)
 
     print("\nРейтинг стран по количеству пользователей-редакторов:")
     for country, count in country_counter.most_common():
         print(f"{country}: {count}")
+
+def main():
+    show(ips2)
+    url = "https://ru.wikipedia.org/w/index.php?title=JSON&action=history"
+    html = get_html(url)
+
+    ips = extract_ips(html)
+    print(f"Найдено уникальных IP: {len(ips)}")
+    show((ips))
+
+
+
 
 
 if __name__ == "__main__":
